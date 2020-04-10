b9e7811a4a54f325398ccba7c6364b0fd2696b7a
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-09 23:17:18 -0700

    Add Game Over Text

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 /***************************************/
 const ctx = gameArea.getContext('2d');
 const welcomeText = "Welcome to Aqua Shot";
+const gameOverText = "Game Over";
 
 /***************************************/
 /************ Game Objects *************/
@@ -19,6 +20,13 @@ class Game {
 		ctx.textAlign = "center";
 		ctx.fillStyle = "white";
 		ctx.fillText(welcomeText, gameArea.width / 2, gameArea.height / 2);
+	}
+
+	displayGameOverMessage = () => {
+		ctx.font = "4em 'Permanent Marker'";
+		ctx.textAlign = "center";
+		ctx.fillStyle = "white";
+		ctx.fillText(gameOverText, gameArea.width / 2, gameArea.height / 2);
 	}
 }
 
