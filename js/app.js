36d523dc8f171552350d4468babf5800dac8e3f1
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-09 22:13:15 -0700

    Create Game class and displayWelcomeMessage() method

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+/***************************************/
+/************** Variables **************/
+/***************************************/
+const ctx = gameArea.getContext('2d');
+const welcomeText = "Welcome to Aqua Shot";
 
+/***************************************/
+/************ Game Objects *************/
+/***************************************/
+
+/**************** Game *****************/
+class Game {
+	constructor() {
+
+	}
+
+	displayWelcomeMessage = () => {
+		ctx.font = "4em 'Permanent Marker'";
+		ctx.textAlign = "center";
+		ctx.fillStyle = "white";
+		ctx.fillText(welcomeText, gameArea.width / 2, gameArea.height / 2);
+	}
+}
+
+let game = new Game();
+game.displayWelcomeMessage();
