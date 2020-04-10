2a9e47c9a24e118a373f8ac2570474af1c0938d0
Jaskaran Singh <jaskaran1523@gmail.com>
2020-04-10 01:19:09 -0700

    Add characters and characterText

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@
 const ctx = gameArea.getContext('2d');
 const welcomeText = "Welcome to Aqua Shot";
 const gameOverText = "Game Over";
+const characterText = "Choose a character";
+
+let characterFlag = 0;
 
 /***************************************/
 /************** Functions **************/
@@ -12,13 +15,25 @@ const clearScreen = () => {
 	ctx.clearRect(0, 0, gameArea.width, gameArea.height);
 };
 
+const showCharacterScreen = () => {
+	game.displayCharacterMessage();
+};
+
 /***************************************/
 /*************** Events ****************/
 /***************************************/
 playBtn.addEventListener("click", () => {
 	clearScreen();
 	playBtn.remove();
-	// showCharacterScreen();
+	showCharacterScreen();
+
+	if (characterFlag == 0) {
+		characters.style.display = "block";
+		characterFlag = 1;
+	} else {
+		characters.style.display = "none";
+		characterFlag = 0;
+	}
 });
 
 /***************************************/
@@ -43,6 +58,13 @@ class Game {
 		ctx.textAlign = "center";
 		ctx.fillStyle = "white";
 		ctx.fillText(gameOverText, gameArea.width / 2, gameArea.height / 2);
+	}
+
+	displayCharacterMessage = () => {
+		ctx.font = "4em 'Permanent Marker'";
+		ctx.textAlign = "center";
+		ctx.fillStyle = "white";
+		ctx.fillText(characterText, gameArea.width / 2, (gameArea.height / 2) - 100);
 	}
 }
 
